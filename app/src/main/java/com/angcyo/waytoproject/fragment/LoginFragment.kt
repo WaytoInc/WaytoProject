4f8f6f9a0ae18c2b6fc8717e7d7f28ae5fa93091
angcyo <angcyo@126.com>
2019-09-29 11:33:50 +0800

    + init

---
+++ app/src/main/java/com/angcyo/waytoproject/fragment/LoginFragment.kt
@@ -0,0 +1,49 @@
+package com.angcyo.waytoproject.fragment
+
+import android.os.Bundle
+import com.angcyo.waytoproject.BuildConfig
+import com.angcyo.waytoproject.R
+import com.wayto.ui.base.main.SaasLoginFragment
+import com.wayto.ui.core.app
+import com.wayto.ui.core.only
+import com.wayto.ui.kotlin.dpi
+import com.wayto.ui.recycler.RBaseViewHolder
+import com.wayto.ui.widget.Button
+
+/**
+ *
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2019/09/23
+ * Copyright (c) 2019 ShenZhen O&M Cloud Co., Ltd. All rights reserved.
+ */
+
+class LoginFragment : SaasLoginFragment() {
+    companion object {
+        val IS_LOGIN_SUCCESS = "${app().scheme}_IS_LOGIN_SUCCESS"
+    }
+
+    override fun getLogoLayout(): Int {
+        return -1
+    }
+
+    override fun getVersionName(): String? {
+        return BuildConfig.VERSION_NAME
+    }
+
+    override fun login(name: String, password: String, company: String) {
+        only(MainFragment())
+    }
+
+    override fun initBaseView(
+        viewHolder: RBaseViewHolder,
+        arguments: Bundle?,
+        savedInstanceState: Bundle?
+    ) {
+        super.initBaseView(viewHolder, arguments, savedInstanceState)
+
+        viewHolder.button(R.id.login_button).apply {
+            setButtonStyle(Button.ROUND_GRADIENT_RECT, 45 * dpi)
+        }
+    }
+}
